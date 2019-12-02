b38ad571b60e15897b9ba6245d450016aa78948b
Dmitry Ascended <DieMon91@list.ru>
2019-12-02 14:53:53 +0200

    commit #1: Field base component created

---
+++ src/components/Field/Field.js
@@ -0,0 +1,9 @@
+import { React, Component } from 'react'
+
+export default class Field extends Component {
+    render() {
+        return(
+            <h1>You are the best!</h1>
+        )
+    }
+}
