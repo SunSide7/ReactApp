7e51e6b21db7446cc4f6de1ee415b28cde10a2c6
Dmitry Ascended <DieMon91@list.ru>
2019-12-02 15:37:05 +0200

    commit #4: Field Component base markup created

--- src/components/Field/Field.js
+++ src/components/Field/Field.js
@@ -3,7 +3,10 @@ import React, { Component } from 'react'
 export default class Field extends Component {
     render() {
         return(
-            <h1>You are the best!</h1>
+            <div className="text-field">
+                <div className="text-field__content">Type any text here</div>
+                <input type="text" className="text-field__input"/>
+            </div>
         )
     }
 }
