49e65baa26cdbeca44e9c9622ea0c8ae06e8b5a6
Dmitry Ascended <DieMon91@list.ru>
2019-12-02 16:06:27 +0200

    commit #10: Field Component state created

--- src/components/Field/Field.js
+++ src/components/Field/Field.js
@@ -10,6 +10,10 @@ export default class Field extends Component {
 
     content = React.createRef()
     input = React.createRef()
+
+    state = {
+        typing: false
+    }
     
     render() {
         return(
