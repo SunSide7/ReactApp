e465a0903a5def2817e4fd8d9417d852db642e22
Dmitry Ascended <DieMon91@list.ru>
2019-12-02 15:47:54 +0200

    commit #6: Field Component refs created

--- src/components/Field/Field.js
+++ src/components/Field/Field.js
@@ -2,11 +2,15 @@ import React, { Component } from 'react'
 import './Field.sass'
 
 export default class Field extends Component {
+
+    content = React.createRef()
+    input = React.createRef()
+    
     render() {
         return(
             <div className="text-field">
-                <div className="text-field__content">Type any text here</div>
-                <input type="text" className="text-field__input"/>
+                <div ref={this.content} className="text-field__content">Type any text here</div>
+                <input ref={this.input} type="text" className="text-field__input"/>
             </div>
         )
     }
