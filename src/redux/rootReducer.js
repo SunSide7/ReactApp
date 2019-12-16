b4fa369e94864fef9be457255e1374ea03059815
Dmitry Ascended <DieMon91@list.ru>
2019-12-16 20:23:40 +0200

    commit #54: Root Reducer Commented Code deleted

--- src/redux/rootReducer.js
+++ src/redux/rootReducer.js
@@ -61,12 +61,7 @@ export default function rootReducer(state = initialState, action) {
             }
             case 'CHANGE':
                 
-                // const idChanged = state.outletInfo.findIndex(item => JSON.stringify(item) === JSON.stringify(action.item))
-                const idChanged = action.index
-                
-                // console.log('Item from reducer:', state.outletInfo[idChanged])
-                // console.log('CHANGE action', action.value, action.item, action.key, idChanged)
-
+                const idChanged = action.index                
                 const itemChanged = {}
 
                 for (let key in state.outletInfo[idChanged]) {
